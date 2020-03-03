1e5c890b7adb27314e3ca732fb6aed4f19fbe883
HE-Xinyu <hxy1303@gmail.com>
2020-03-04 00:39:29 +0800

    feat(push-relabel): a base class

--- src/max_flow.cpp
+++ src/max_flow.cpp
@@ -95,3 +95,70 @@ public:
 		return ret;
 	}
 };
+
+
+template<typename Flow>
+class GeneralPushRelabel : public MaxFlowBase<Flow> {
+protected:
+	// (f, h) instance
+	vector<vector<Flow>> preFlow;
+	vector<int> height;
+	// save the excess amount to save computation cost
+	vector<Flow> excess;
+public:
+	GeneralPushRelabel(vector<vector<int>> _adj, vector<vector<Flow>> _cap, int _s, int _t) : MaxFlowBase<Flow>(_adj, _cap, _s, _t) {
+		size_t n = _cap.size();
+		preFlow = vector<vector<Flow>>(n, vector<Flow>(n, 0));
+		height = vector<int>(n, 0);
+		excess = vector<Flow>(n, 0);
+		/*
+		 * standard initialization.
+		 * the start vertex will have height of n.
+		 * make (s, u) have zero residual capacity.
+		 */
+		height[_s] = n;
+		for (int v : _adj[_s]) {
+			preFlow[_s][v] += _cap[_s][v];
+			excess[v] += _cap[_s][v];
+		}
+	}
+
+	void push(int u, int v) {
+		/*
+		 * h(u) = h(v) + 1, and edge (u, v) has residual capacity.
+		 * Push flow from u to v as much as we can.
+		 *
+		 * The time complexity is O(1).
+		 * 
+		 * We apply a simple greedy strategy: first decrease f(v, u) as much as we can, then increase f(u, v).
+		 */
+		Flow amount = min(this->cap[u][v] - preFlow[u][v] + preFlow[v][u], excess[u]);
+		excess[u] -= amount;
+		excess[v] += amount;
+		Flow amountDecrease = min(amount, preFlow[v][u]);
+		preFlow[v][u] -= amountDecrease;
+		preFlow[u][v] += (amount - amountDecrease);
+	}
+
+	void relabel(int u) {
+		/*
+		 * Do a relabel on u.
+		 * 
+		 * Find the lowest vertex v satisfying c_f(u, v) > 0
+		 * It should be guaranteed that such v exists, and h(u) <= h(v) for all v before relabel.
+		 *
+		 * Update h(u) = h(v) + 1
+		 * 
+		 * Time complexity: O(d(u))
+		 */
+
+		int mn = INT_MAX;
+		for (int v : this->adj[u]) {
+			if (this->cap[u][v] - preFlow[u][v] + preFlow[v][u] > 0) {
+				mn = min(mn, height[v]);
+			}
+		}
+
+		height[u] = mn + 1;
+	}
+};
